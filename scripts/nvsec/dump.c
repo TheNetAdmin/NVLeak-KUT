7e65ed26196ab681993a7a94413d0ef5585d3926
Zixuan Wang <zxwang42@gmail.com>
2021-10-16 15:39:12 -0700

    Dump covert data to dax device

--- scripts/nvsec/dump.c
+++ scripts/nvsec/dump.c
@@ -10,7 +10,8 @@
 
 static void usage(void)
 {
-	printf("Usage: dump -f file_path [-d write_data]\n");
+	printf("Usage: dump -o out_file_path [-d write_data]\n");
+	printf("Usage: dump -o out_file_path [-i in_file_path -s size_in_byte]\n");
 }
 
 static void read_data_pmem(uint64_t *ptr, int count)
@@ -28,44 +29,9 @@ static void write_data_pmem(uint64_t *ptr, uint64_t data)
 	_mm_mfence();
 }
 
-int main(int argc, char *argv[])
+static void write_file_pmem(uint64_t *ptr, char *file_path, size_t file_size)
 {
-	int opt;
-
-	char *	 file_path;
-	uint64_t write_data = 0;
-	enum { READ,
-	       WRITE,
-	} operation = READ;
-
-	while ((opt = getopt(argc, argv, "f:d:")) != -1) {
-		switch (opt) {
-		case 'f':
-			file_path = optarg;
-			break;
-		case 'd':
-			write_data = strtoull(optarg, NULL, 0);
-			operation  = WRITE;
-			break;
-		default:
-			usage();
-			exit(1);
-			break;
-		}
-	}
-
-	if (file_path == NULL) {
-		usage();
-		exit(1);
-	}
-
-	printf("Args:\n");
-	printf("    - file_path : %s\n", file_path);
-	printf("    - opeeration: %u\n", operation);
-	printf("    - write_data: 0x%016lx\n", write_data);
-
-	printf("Opening file %s\n", file_path);
-	int fd = open(file_path, O_RDWR);
+	int fd = open(file_path, O_RDONLY);
 	if (fd < 0) {
 		printf("Could not open file: %s\n", file_path);
 		exit(2);
@@ -75,6 +41,93 @@ int main(int argc, char *argv[])
 	int	    err = fstat(fd, &statbuf);
 	if (err < 0) {
 		printf("Could not stat file: %s\n", file_path);
+		exit(2);
+	}
+
+	if (file_size > statbuf.st_size) {
+		printf("File size mismatch, need %lu byte, but file only has %lu bytes\n",
+		       file_size, statbuf.st_size);
+		exit(2);
+	}
+
+	uint8_t *in_ptr = (uint8_t *)mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
+	if (in_ptr == MAP_FAILED) {
+		printf("Mapping failed\n");
+		exit(2);
+	}
+	close(fd);
+
+	uint8_t *out_ptr = (uint8_t *)ptr;
+
+	for (size_t i = 0; i < file_size; i++) {
+		out_ptr[i] = in_ptr[i];
+	}
+
+	err = munmap(in_ptr, file_size);
+	if (err != 0) {
+		printf("Unmapping Failed\n");
+		exit(2);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int opt;
+
+	char *	 out_file_path;
+	char *	 in_file_path;
+	size_t	 in_file_size;
+	uint64_t write_data = 0;
+	enum { READ,
+	       WRITE_DATA,
+	       WRITE_FILE,
+	} operation = READ;
+
+	while ((opt = getopt(argc, argv, "o:d:i:s:")) != -1) {
+		switch (opt) {
+		case 'o':
+			out_file_path = optarg;
+			break;
+		case 'd':
+			write_data = strtoull(optarg, NULL, 0);
+			operation  = WRITE_DATA;
+			break;
+		case 'i':
+			in_file_path = optarg;
+			operation    = WRITE_FILE;
+			break;
+		case 's':
+			in_file_size = strtoul(optarg, NULL, 0);
+			break;
+		default:
+			usage();
+			exit(1);
+			break;
+		}
+	}
+
+	if (out_file_path == NULL) {
+		printf("ERROR: missing argument out_file_path\n");
+		usage();
+		exit(1);
+	}
+
+	printf("Args:\n");
+	printf("    - out_file_path : %s\n", out_file_path);
+	printf("    - opeeration: %u\n", operation);
+	printf("    - write_data: 0x%016lx\n", write_data);
+
+	printf("Opening file %s\n", out_file_path);
+	int fd = open(out_file_path, O_RDWR);
+	if (fd < 0) {
+		printf("Could not open file: %s\n", out_file_path);
+		exit(2);
+	}
+
+	struct stat statbuf;
+	int	    err = fstat(fd, &statbuf);
+	if (err < 0) {
+		printf("Could not stat file: %s\n", out_file_path);
 		exit(2);
 	}
 
@@ -100,15 +153,20 @@ int main(int argc, char *argv[])
 
 	read_data_pmem(ptr, 4);
 
-	if (operation == WRITE) {
+	if (operation == WRITE_DATA) {
 		printf("Writing data [0x%016lx] to [%p]\n", write_data, ptr);
 		write_data_pmem(&(ptr[0]), write_data);
 		read_data_pmem(ptr, 4);
+	} else if (operation == WRITE_FILE) {
+		printf("Writing file [%s] (size [%lu]) to [%p]\n", in_file_path,
+		       in_file_size);
+		write_file_pmem(ptr, in_file_path, in_file_size);
+		read_data_pmem(ptr, (in_file_size / 8) + 1);
 	}
 
 	err = munmap(ptr, file_size);
 	if (err != 0) {
-		printf("UnMapping Failed\n");
+		printf("Unmapping Failed\n");
 		exit(2);
 	}
 
