7ba4fd4c87b746f9e3cbb3196261f8f43beb5cd5
Zixuan Wang <zxwang42@gmail.com>
2021-10-18 20:56:43 -0700

    Probe all possible offsets

--- scripts/nvsec/covert.sh
+++ scripts/nvsec/covert.sh
@@ -3,7 +3,6 @@
 set -e
 
 batch_id="$(date +%Y%m%d%H%M%S)"
-batch_result_dir="results/${batch_id}"
 
 script_root=$(realpath $(realpath $(dirname $0)))
 source "$script_root/utils/slack.sh"
@@ -36,26 +35,26 @@ block_size=64
 stride_array=(
 	# $((2 ** 16))
 	# $((2 ** 17))
-	$((2 ** 18))
-	$((2 ** 19))
-	$((2 ** 20)) # 2 ** 20 --> 1 MiB == 16MiB/16Way
+	# $((2 ** 18))
+	# $((2 ** 19))
+	# $((2 ** 20)) # 2 ** 20 --> 1 MiB == 16MiB/16Way
 	$((2 ** 21))
-	$((2 ** 22))
+	# $((2 ** 22))
 	# $((2 ** 23))
 	# $((2 ** 24))
 )
 region_array=(
 	# $((2 **  7))
 	# $((2 **  8))
-	$((2 **  9))
+	# $((2 **  9))
 	# $(seq -s ' ' $((2 ** 9)) $((2 ** 6)) $((2 ** 10 - 1)))
-	$((2 ** 10)) # 16 * 64 --> 16 blocks --> 16 way
+	# $((2 ** 10)) # 16 * 64 --> 16 blocks --> 16 way
 	$((2 ** 11))
-	$((2 ** 12))
-	$((2 ** 13))
+	# $((2 ** 12))
+	# $((2 ** 13))
 )
 sub_op_array=(1) # Covert channel: Pointer chasing read only
-repeat_array=(8 16 32)
+repeat_array=(16)
 region_align=4096
 
 fence_strategy_array=(0)
@@ -63,7 +62,7 @@ fence_freq_array=(1)
 flush_after_load_array=(0 1)
 record_timing_array=(1) # 1: per_reapeat
 flush_l1_array=(0 1)
-receiver_page_offset_array=($(seq -s ' ' 0 15))
+receiver_page_offset_array=($(seq -s ' ' 0 255))
 
 function run_qemu() {
 	# $1 sender | receiver
@@ -230,11 +229,11 @@ debug)
 	flush_after_load_array=(1)
 	receiver_page_offset_array=($(seq -s ' ' 0 15))
 	export no_slack=1
-	batch_result_dir="${batch_result_dir}/${job}"
+	batch_result_dir="results/${job}/${batch_id}"
 	bench_func
 	;;
 all)
-	batch_result_dir="${batch_result_dir}/${job}"
+	batch_result_dir="results/${job}/${batch_id}"
 	bench_func
 	;;
 *)
